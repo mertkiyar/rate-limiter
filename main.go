b5818ea62db49b9f8f388bceaface8c9e81f6e3c
mertkiyar <mertkiyar69@gmail.com>
2026-08-27 16:16:29 +0300

    feat: read and extract client IP address

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		// r.RemoteAddr includes ip and port number
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			http.Error(w, "Server Error", http.StatusInternalServerError)
+			return
+		}
 		fmt.Fprintln(w, "API Rate Limiter working good!")
+		fmt.Fprintf(w, "Your IP Address with port: %s\n", r.RemoteAddr) // with port
+		fmt.Fprintf(w, "Your IP Address: %s", ip)                       // without port
 	})
 
 	fmt.Println("API Rate Limiter working on 8080 port")
