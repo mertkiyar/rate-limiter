ed122f6de0215992ea24e4675c0a74103c601ddd
mertkiyar <mertkiyar69@gmail.com>
2026-08-27 16:08:16 +0300

    feat: setup basic http server

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "API Rate Limiter working good!")
+	})
+
+	fmt.Println("API Rate Limiter working on 8080 port")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("API Rate Limiter not started", err)
+	}
 }
