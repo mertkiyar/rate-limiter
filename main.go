1c747a4c1b2542a69271f9beebd48c13e3dc7f0f
mertkiyar <mertkiyar69@gmail.com>
2026-08-27 16:23:44 +0300

    feat: implement basic ip request counting and limiting

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 )
 
+// map[ip]count format
+var visitors = make(map[string]int)
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
@@ -15,9 +18,19 @@ func main() {
 			http.Error(w, "Server Error", http.StatusInternalServerError)
 			return
 		}
+
+		// increse visit count with each login
+		visitors[ip]++
+
+		if visitors[ip] > 3 {
+			http.Error(w, "Limit exceeded", http.StatusTooManyRequests)
+			return
+		}
+
 		fmt.Fprintln(w, "API Rate Limiter working good!")
 		fmt.Fprintf(w, "Your IP Address with port: %s\n", r.RemoteAddr) // with port
-		fmt.Fprintf(w, "Your IP Address: %s", ip)                       // without port
+		fmt.Fprintf(w, "Your IP Address: %s\n", ip)                     // without port
+		fmt.Fprintf(w, "Visit Count: %d", visitors[ip])                 // visit count
 	})
 
 	fmt.Println("API Rate Limiter working on 8080 port")
